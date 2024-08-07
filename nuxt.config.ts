5cd042812c304228418ff04f0eec1dab514ab6e5
Cody Bontecou <bontecouc@gmail.com>
2024-08-07 11:43:48 +0800

    env vars

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', "@nuxt/icon"],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon'],
+  runtimeConfig: {
+    CLAUDE_API_KEY: process.env.CLAUDE_API_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
 })
