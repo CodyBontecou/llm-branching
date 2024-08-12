ea44a2b489d3469375b929eb71664e862ed7181e
Cody Bontecou <bontecouc@gmail.com>
2024-08-12 20:45:04 +0800

    removed pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon', "@pinia/nuxt"],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxt/icon'],
   runtimeConfig: {
     public: {
       CLAUDE_API_KEY: process.env.CLAUDE_API_KEY,
